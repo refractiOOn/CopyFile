e6f333c07c7fd758045f1393a69d85d44637b871
refractioN <etozhesotnikow@gmail.com>
2021-11-17 14:23:43 +0200

    copy file function actualized

--- CopyFile.cpp
+++ CopyFile.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <Windows.h>
 
 int main()
 {
-
+	LPCSTR existingFileName = "D:\\test.txt";
+	LPCSTR newFileName = "D:\\Test\\test.txt";
+	BOOL funcSuccess = CopyFileA(
+		existingFileName,
+		newFileName,
+		false
+	);
+	if (!funcSuccess)
+	{
+		std::cout << "Something went wrong" << std::endl;
+	}
 }
