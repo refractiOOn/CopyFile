bfa9797c318b604866c7c6230c37ddef0d7d6982
refractioN <etozhesotnikow@gmail.com>
2021-11-17 17:22:51 +0200

    strange task solution

--- CopyFile.cpp
+++ CopyFile.cpp
@@ -5,15 +5,65 @@
 
 int main()
 {
-	LPCSTR existingFileName = "D:\\test.txt";
-	LPCSTR newFileName = "D:\\Test\\test.txt";
-	BOOL funcSuccess = CopyFileA(
-		existingFileName,
-		newFileName,
-		false
-	);
-	if (!funcSuccess)
+	try
 	{
-		std::cout << "Something went wrong" << std::endl;
+		LPCSTR newFileName;
+		BOOL funcSuccess = CopyFileA(
+			"D:\\test.txt",
+			"D:\\Test\\test.txt",
+			false
+		);
+		if (!funcSuccess)
+		{
+			throw std::exception();
+		}
+
+		std::vector<std::string> v;
+		std::fstream file;
+
+		file.open("D:\\Test\\Test.txt");
+		if (!file.is_open())
+		{
+			throw std::exception();
+		}
+		std::string temp;
+		while (file >> temp)
+		{
+			v.push_back(temp);
+		}
+
+		auto password = find(v.begin(), v.end(), "some_password");
+		if (password == v.end())
+		{
+			std::cout << "\"some_password\" not found" << std::endl;
+		}
+		else
+		{
+			*password = "123456789";
+		}
+
+		file.open("D:\\Test\\test.txt", std::fstream::out);
+		if (!file.is_open())
+		{
+			throw std::exception();
+		}
+		for (size_t i = 0; i < v.size(); ++i)
+		{
+			file << v[i];
+			if (i % 2 != 0)
+			{
+				file << "\n";
+			}
+			else
+			{
+				file << " ";
+			}
+		}
+
+		file.close();
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
 	}
 }
